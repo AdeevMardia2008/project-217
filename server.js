cdc8c8cc3ae733392c41663666641aefb5ff1726
AdeevMardia2008 <71126451+AdeevMardia2008@users.noreply.github.com>
2022-09-06 21:34:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const app = express();
 const server = require("http").Server(app);
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.json())
+
+const { v4: uuidv4 } = require("uuid");
 
 const io = require("socket.io")(server, {
     cors: {
@@ -10,13 +13,57 @@ const io = require("socket.io")(server, {
     }
 });
 
-app.get("/", (req, res) => {
-    res.render("index");
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
 });
 
+app.use("/peerjs", peerServer);
+
+var nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    port: 465,
+    host: "smtp.gmail.com",
+    auth: {
+        user: 'adeevmardia@gmail.com',
+        pass: 'dpwnkeequlxjybfr',
+    },
+    secure: true,
+});
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+
+app.post("/send-mail", (req, res) => {
+    const to = req.body.to;
+    const url = req.body.url;
+    const mailData = {
+        from: "apoorv.goyal@whitehatjr.com",
+        to: to,
+        subject: "You got hacked",
+        html: `<p>lmfao you got hacked</p>`
+    };
+    transporter.sendMail(mailData, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        res.status(200).send({ message: "Invitation sent!", message_id: info.messageId });
+    });
+})
+
 io.on("connection", (socket) => {
-    socket.on("message", (message, user) => {
-        io.emit("createMessage", message, user);
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
     });
 });
 
